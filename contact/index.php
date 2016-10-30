c34b4c6ec5d5ddc11d030214e99763974398f17f
poonkeegan <poonkeegan@gmail.com>
2016-10-29 22:49:25 -0400

    switched to php to include header and footer instead of rewriting

---
+++ contact/index.php
@@ -0,0 +1,14 @@
+<?php
+include($_SERVER['DOCUMENT_ROOT']. '/include/header.html') ;
+?>
+<h1>
+  Ways to get in touch:
+</h1>
+<p>
+  My personal email: <a href="mailto:poonkeegan@gmail.com.com">poonkeegan@gmail.com</a> <br>
+  My school affiliated email: <a href="mailto:keegan.poon@mail.utoronto.ca">keegan.poon@mail.utoronto.ca</a> <br>
+  My phone number
+</p>
+<?php
+include($_SERVER['DOCUMENT_ROOT']. '/include/footer.html');
+?>
---
+++ hobbies/index.php
@@ -0,0 +1,14 @@
+<?php
+include($_SERVER['DOCUMENT_ROOT']. '/include/header.html') ;
+?>
+<h1>
+  Stuff I like to do:
+</h1>
+<p>
+  <a href="https://github.com/poonkeegan">Github</a> <br>
+  <a href="https://myanimelist.net/animelist/temeyun">M.A.L.</a> <br>
+  <a href="http://steamcommunity.com/id/temeyun">Steam</a> <br>
+</p>
+<?php
+include($_SERVER['DOCUMENT_ROOT']. '/include/footer.html');
+?>
